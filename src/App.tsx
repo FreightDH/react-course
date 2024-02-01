1f722ef5ef5e55227e3f1a01c5bb0dd7b9bf1e95
FreightDH <maxim.zolotkov@yandex.ru>
2024-02-01 21:22:23 +0300

    init: start project 02-tours

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import { ReactElement } from 'react';
+
+const URL = 'https://course-api.com/react-tours-project';
+
+const App = (): ReactElement => {
+  return <h2>Tours Starter</h2>;
+};
+
+export default App;
