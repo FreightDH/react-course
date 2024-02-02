b28d9d50b594110220b58c86922572fc28068bb9
FreightDH <maxim.zolotkov@yandex.ru>
2024-02-02 11:56:00 +0300

    feat: add read more or show less button

--- src/components/Tour.tsx
+++ src/components/Tour.tsx
@@ -1,17 +1,24 @@
-import { FC, ReactElement } from 'react';
+import { FC, ReactElement, useState } from 'react';
 
 interface TourProps {
   tour: Tour;
 }
 
 const Tour: FC<TourProps> = ({ tour }): ReactElement => {
+  const [moreInfo, setMoreInfo] = useState(false);
+
   return (
     <li className="single-tour">
       <div className="tour-price">${tour.price}</div>
       <img src={tour.image} alt={tour.name} className="img" />
       <div className="tour-info">
         <h5>{tour.name}</h5>
-        <p>{tour.info}</p>
+        <p>
+          {moreInfo ? tour.info : `${tour.info.slice(0, 200)}...`}
+          <button className="info-btn" onClick={() => setMoreInfo(!moreInfo)}>
+            {moreInfo ? 'Show Less' : 'Read More'}
+          </button>
+        </p>
       </div>
     </li>
   );
