3a39e45e4bf40658e5efaf89a32938968f783b52
FreightDH <maxim.zolotkov@yandex.ru>
2024-02-02 14:45:40 +0300

    feat: add review markup

--- 03-reviews/src/App.tsx
+++ 03-reviews/src/App.tsx
@@ -1,7 +1,35 @@
-import { ReactElement } from 'react';
+import { ReactElement, useState } from 'react';
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
+import reviews from './data';
 
 const App = (): ReactElement => {
-  return <h2>Reviews Starter</h2>;
+  const [activeIndex, setActiveIndex] = useState(0);
+  const { name, job, image, text } = reviews[activeIndex];
+
+  return (
+    <main>
+      <div className="review">
+        <div className="img-container">
+          <img src={image} alt={name} className="person-img" />
+          <div className="quote-icon">
+            <FaQuoteRight />
+          </div>
+        </div>
+        <h4 className="author">{name}</h4>
+        <p className="job">{job}</p>
+        <p className="info">{text}</p>
+        <div className="btn-container">
+          <button className="prev-btn">
+            <FaChevronLeft />
+          </button>
+          <button className="next-btn">
+            <FaChevronRight />
+          </button>
+        </div>
+        <button className="btn btn-hipster">surprise me</button>
+      </div>
+    </main>
+  );
 };
 
 export default App;
