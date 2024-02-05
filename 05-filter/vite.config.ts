fa0f98beaa60191be999a0a391477e17abd38939
FreightDH <maxim.zolotkov@yandex.ru>
2024-02-05 11:42:01 +0300

    chore: change config

--- 05-filter/vite.config.ts
+++ 05-filter/vite.config.ts
@@ -4,9 +4,4 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   base: '',
   plugins: [react()],
-  resolve: {
-    alias: {
-      components: '/src/components',
-    },
-  },
 });
