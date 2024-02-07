5a4d0b2ccb75cce20f396e26d8995f45f75e7724
FreightDH <maxim.zolotkov@yandex.ru>
2024-02-07 13:27:45 +0300

    feat: update App component

--- 06-filter/src/App.tsx
+++ 06-filter/src/App.tsx
@@ -1,9 +1,22 @@
-import { ReactElement } from 'react';
+import { ReactElement, useState } from 'react';
+import { useFetch } from './shared';
+import { Buttons, Job, Loader } from './components';
 
 const URL = 'https://course-api.com/react-tabs-project';
 
 const App = (): ReactElement => {
-  return <></>;
+  const [jobs, isLoading, isError] = useFetch(URL);
+  const [activeTab, setActiveTab] = useState(0);
+
+  if (isLoading) return <Loader />;
+  if (!jobs || isError) return <h3>Something wen't wrong!</h3>;
+
+  return (
+    <main className="jobs-center">
+      <Buttons jobs={jobs} activeTab={activeTab} setActiveTab={setActiveTab} />
+      <Job job={jobs.find((_, index) => index === activeTab)} />
+    </main>
+  );
 };
 
 export default App;
