0a654cccb45c8b83aee80c7b283d0a5c35403bc1
FreightDH <maxim.zolotkov@yandex.ru>
2024-02-09 12:28:00 +0300

    feat: update App component

--- 07-slider/src/App.tsx
+++ 07-slider/src/App.tsx
@@ -1,15 +1,47 @@
-import { ReactElement } from 'react';
-import cl from './App.module.scss';
+import { ReactElement, useEffect, useState } from 'react';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+import { longList } from './data';
+import Slide from './Slide';
 
 const App = (): ReactElement => {
+  const [people] = useState(longList);
+  const [currentPerson, setCurrentPerson] = useState(0);
+
+  const prevSlide = () => {
+    setCurrentPerson((prevPerson) => {
+      const result = (prevPerson - 1 + people.length) % people.length;
+      return result;
+    });
+  };
+
+  const nextSlide = () => {
+    setCurrentPerson((prevPerson) => {
+      const result = (prevPerson + 1) % people.length;
+      return result;
+    });
+  };
+
+  useEffect(() => {
+    const sliderId = setInterval(() => {
+      nextSlide();
+    }, 5000);
+    return () => {
+      clearInterval(sliderId);
+    };
+  }, [currentPerson]);
+
   return (
-    <>
-      <main className={cl.page}>
-        <div className="page__container">
-          <div className={cl.page__body}></div>
-        </div>
-      </main>
-    </>
+    <section className="slider-container">
+      {people.map((person, index) => (
+        <Slide person={person} currentPerson={currentPerson} index={index} key={person.id} />
+      ))}
+      <button type="button" className="prev" onClick={prevSlide}>
+        <FiChevronLeft />
+      </button>
+      <button type="button" className="next" onClick={nextSlide}>
+        <FiChevronRight />
+      </button>
+    </section>
   );
 };
 
