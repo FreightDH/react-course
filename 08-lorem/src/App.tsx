935610ec234bbbc511dd09c05be3914b9ff93e93
Max Zolotkov <maxim.zolotkov@yandex.ru>
2024-03-04 11:30:43 +0300

    feat: add functional

--- 08-lorem/src/App.tsx
+++ 08-lorem/src/App.tsx
@@ -1,15 +1,42 @@
-import { ReactElement } from 'react';
-import cl from './App.module.scss';
+import { ChangeEvent, FormEvent, ReactElement, useState } from 'react';
+
+import data from './data';
 
 const App = (): ReactElement => {
+  const [count, setCount] = useState(1);
+  const [text, setText] = useState<string[]>([]);
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const { target } = event;
+    setCount(+target.value);
+  };
+
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    setText(data.slice(0, count));
+  };
+
   return (
-    <>
-      <main className={cl.page}>
-        <div className="page__container">
-          <div className={cl.page__body}></div>
-        </div>
-      </main>
-    </>
+    <section className="section-center">
+      <h4>TIRED OF BORING LOREM IPSUM?</h4>
+      <form action="#" className="lorem-form" onSubmit={handleSubmit}>
+        <label htmlFor="numberInput">Paragraphs: </label>
+        <input
+          type="number"
+          id="numberInput"
+          min="1"
+          max={data.length}
+          value={count}
+          onChange={handleChange}
+        />
+        <button className="btn">Generate</button>
+      </form>
+      <div className="lorem-text">
+        {text.map((paragraph, index) => (
+          <p key={index}>{paragraph}</p>
+        ))}
+      </div>
+    </section>
   );
 };
 
